38d614fbfae9dd1853df81d12850d225e1092e84
ifeelbored <ursula771023@gmail.com>
2022-05-19 10:42:16 +0200

    main.cpp

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -9,13 +9,17 @@ int main(void)
     BPlayer.beRepaired(10);
 	APlayer.attack("Bplayer");
     if (APlayer.getAttackDamage() > 0 && APlayer.getEnergyPoints() > 0)
+    {
         BPlayer.takeDamage(APlayer.getAttackDamage());
+    }
 	BPlayer.beRepaired(10);
 	BPlayer.beRepaired(10);
     APlayer.beRepaired(10);
 	BPlayer.attack("Aplayer");
     if (BPlayer.getAttackDamage() > 0 && BPlayer.getEnergyPoints() > 0)
+    {
 	    APlayer.takeDamage(BPlayer.getAttackDamage());
+    }
 	APlayer.guardGate();
 	std::cout<<"Aplayer: attackpoint/hitpoint/energypoint : " 
 			<< APlayer.getAttackDamage() << "/" 
--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -7,16 +7,20 @@ int main(void)
 	FragTrap	APlayer("Aplayer");
 	FragTrap	BPlayer("Bplayer");
 
-    BPlayer.beRepaired(10);
+   BPlayer.beRepaired(10);
 	APlayer.attack("Bplayer");
     if (APlayer.getAttackDamage() > 0 && APlayer.getEnergyPoints() > 0)
+    {
         BPlayer.takeDamage(APlayer.getAttackDamage());
+    }
 	BPlayer.beRepaired(10);
 	BPlayer.beRepaired(10);
     APlayer.beRepaired(10);
 	BPlayer.attack("Aplayer");
     if (BPlayer.getAttackDamage() > 0 && BPlayer.getEnergyPoints() > 0)
+    {
 	    APlayer.takeDamage(BPlayer.getAttackDamage());
+    }
 	APlayer.highFivesGuys();
 	std::cout<<"Aplayer: attackpoint/hitpoint/energypoint : " 
 			<< APlayer.getAttackDamage() << "/" 
