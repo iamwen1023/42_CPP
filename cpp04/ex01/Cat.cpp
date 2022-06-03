a4b316ff9a04674a59928fbd09f27afaa2e63995
ifeelbored <ursula771023@gmail.com>
2022-06-03 11:30:52 +0200

    assignment =

--- cpp04/ex01/Cat.cpp
+++ cpp04/ex01/Cat.cpp
@@ -10,8 +10,6 @@ Cat::Cat(const Cat &src):Animal()
 {
     std::cout << "Cat Copy constructor called" << std::endl;
     this->type = src.getType();
-    if (this->catBrain)
-        delete catBrain;
     this->catBrain = new Brain();
     int i = 0;
     while(!src.catBrain->ideas[i].empty() && i < 100)
@@ -31,6 +29,8 @@ Cat &Cat::operator=(Cat const & rhs)
 {
     std::cout << "Cat Assignment operator called" << std::endl;
     this->type = rhs.getType();
+    if (this->catBrain)
+        delete catBrain;
     this->catBrain = new Brain();
     // int i = 0;
     // while(!rhs.catBrain->ideas[i].empty() && i < 100)
